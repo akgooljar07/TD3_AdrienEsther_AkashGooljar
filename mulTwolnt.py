921547bbf58574f2229bde4894c5acc11778c20a
Adrien2305 <alesther@student.udm.ac.mu>
2021-03-16 10:16:44 +0000

    [adrien] ceci cest un programme de multiplication

---
+++ mulTwolnt.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def mul(x,y):
+    return x*y
+
+num1 = int(input("Entrez num 1 svp: "))
+
+num2 = int(input("Entrez num 2 svp: "))
+
+z = mul(num1 , num2)
+
+print("The mul :", z)
+
