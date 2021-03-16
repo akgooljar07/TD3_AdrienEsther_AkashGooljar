2ec7b187a8d52820f41dea7a66616a95d27b7c73
Master <agnetdesign@gmail.com>
2021-03-16 20:24:46 +0400

    [add]-Deuxieme Modifications

--- addTwoInt.py
+++ addTwoInt.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
+
+
 def add(x,y):
 	return x+y
 
 try:
-	num1,num2= (input("enter 2 number")).split( )
-	w=int(num1)
-	n=int(num2)
-	z = add(w,n)
-	print("The SUM : ",z)
+
+   num1,num2=(input("enter 2 number")).split( )
+   w=int(num1)
+   n=int(num2)
+   z = add(w,n)
+   print("The SUM : ",z)
 
 except (ValueError):
-	print("Please input only 2 numbers only !")
+   print("Please input  2 numbers !")
 
-
+   num1,num2=(input("enter 2 number")).split( )
+   w=int(num1)
+   n=int(num2)
+   z = add(w,n)
+   print("The SUM : ",z)
