7b2760e57a7eb3f36626afa729f56994a0ab6a48
Master <agnetdesign@gmail.com>
2021-03-20 23:18:12 +0400

    [integration] 1.0

--- main.py
+++ main.py
@@ -4,28 +4,32 @@
 def add(x,y):
 	return x+y
 
+def mul(x,y):
+    return x*y
+
+
 try:
 
    num1,num2=(input("enter 2 number : ")).split( )
    w=int(num1)
    n=int(num2)
-   choice = input("Do you want to add the two integers - Type yes / no :  ")
-   z=add(w,n)
-
+   print("Choose Your Operation!")
+   print("1.Addition")
+   print("2.Multiplication")
+   choice = int(input("Choose Your Operation : "))
 except (ValueError):
 
    print("Please input  2 numbers !")
-   num1,num2=(input("enter 2 number")).split( )
+   num1,num2=(input("enter 2 number : ")).split( )
    w=int(num1)
    n=int(num2)
-   z = add(w,n)
-   choice = input("Do you want to add the two integers - Type yes / no  :  ")
+   choice = int(input("Choose Your Operation :  "))
 
 
-if (choice=="yes"):
-
+if (choice=="1"):
+  z=add(w,n)
   print("The sum : ",z)
 
 else:
-    print("Your num1 is : ", w)
-    print("Your num2 is : ", n)
+    m=mul(w,n)
+    print("The Multiplication : ",m)
