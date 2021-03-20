8ceb9c0cf1bb0c78922b636281412bdf9adcecce
Adrien2305 <alesther@student.udm.ac.mu>
2021-03-20 18:00:11 +0000

    [integration]-first modifications

--- mulTwolnt.py
+++ mulTwolnt.py
@@ -3,11 +3,28 @@
 def mul(x,y):
     return x*y
 
-num1 = int(input("Entrez num 1 svp: "))
 
-num2 = int(input("Entrez num 2 svp: "))
 
-z = mul(num1 , num2)
+try:
 
-print("The mul :", z)
+    num1,num2=(input("Entrez 2 nombres :")).split( )
+    w=int(num1)
+    n=int(num2)
+    z=mul(w,n)
+    print("The mul : ", z)
+
+except (ValueError):
+    print("please entrez 2 nombres svp :")
+    num1,num2=(input("Entrez 2 nombres :")).split( )
+    w=int(num1)
+    n=int(num2)
+    z=mul(w,n)
+    print("The mul : ", z)
+except (ValueError):
+    print("please entrez 2 nombres svp :")
+    num1,num2=(input("Entrez 2 nombres :")).split( )
+    w=int(num1)
+    n=int(num2)
+    z=mul(w,n)
+    print("The mul : ", z)
 
